7726d83fe1e4351d1f5a78a386246dd7b9950f74
yurivfernandes <yuri.viana.fernandes@gmail.com>
2025-09-03 17:50:23 -0300

    refactor: corrigir a ordem das URLs base no arquivo de configuração da API

--- src/src/services/api.js
+++ src/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api', // Verifique se esta URL está correta
-  //baseURL: 'https://api-avaliacao-incidentes.gestri.com.br/api',
+  //baseURL: 'http://localhost:8000/api', // Verifique se esta URL está correta
+  baseURL: 'https://api.gamedesk.com.br/api',
   headers: {
     'Content-Type': 'application/json',
   },
